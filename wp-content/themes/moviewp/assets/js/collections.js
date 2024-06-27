98cf0740c1ebeab7cb15ed0233a408b74a706a27
mantleice <sheikala6490@gmail.com>
2024-06-28 00:56:52 +0530

    update

--- wp-content/themes/moviewp/assets/js/collections.js
+++
@@ -1,1 +0,0 @@
-$("li").hover(function(){$(".collection_item",this).addClass("opacity-0")},function(){$(".collection_item",this).removeClass("opacity-0")});
--- wp-content/themes/moviewp/template-parts/slider/slider-0.php
+++
@@ -1,8 +0,0 @@
-<?php
-get_template_part('template-parts/slider/slider', '1');
-get_template_part('template-parts/slider/slider', '2');
-get_template_part('template-parts/slider/slider', '3');
-get_template_part('template-parts/slider/slider', '4');
-get_template_part('template-parts/slider/slider', '5');
-get_template_part('template-parts/slider/slider', '6');
-get_template_part('template-parts/slider/slider', '7');
