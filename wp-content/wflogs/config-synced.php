a581a755e8974e75123df4dc62d4803f1897daef
mantleice <sheikala6490@gmail.com>
2024-06-28 14:00:36 +0530

    update

---
+++ wp-content/wflogs/config-synced.php
@@ -0,0 +1,6 @@
+<?php exit('Access denied'); __halt_compiler(); ?>
+******************************************************************
+This file is used by the Wordfence Web Application Firewall. Read 
+more at https://docs.wordfence.com/en/Web_Application_Firewall_FAQ
+******************************************************************
+a:19:{s:6:"apiKey";s:0:"";s:6:"isPaid";b:0;s:7:"siteURL";N;s:7:"homeURL";N;s:14:"whitelistedIPs";N;s:9:"howGetIPs";N;s:25:"howGetIPs_trusted_proxies";N;s:33:"howGetIPs_trusted_proxies_unified";N;s:13:"pluginABSPATH";N;s:11:"other_WFNet";b:1;s:9:"serverIPs";N;s:15:"blockCustomText";N;s:13:"timeoffset_wf";N;s:23:"advancedBlockingEnabled";N;s:20:"disableWAFIPBlocking";N;s:13:"patternBlocks";N;s:13:"countryBlocks";N;s:11:"otherBlocks";N;s:8:"lockouts";N;}
