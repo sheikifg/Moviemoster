92924ef6f76365c7b20ada4d07781feceaa72b00
mantleice <sheikala6490@gmail.com>
2024-07-01 07:35:59 +0530

    update

--- wp-content/themes/ace-news/sections/above-footer-widgets.php
+++
@@ -1,7 +0,0 @@
-<?php if ( is_active_sidebar( 'above-footer-widgets-area' ) ) : ?>
-	<div class="above-footer-widgets-area section-splitter">
-		<div class="section-wrapper">
-			<?php dynamic_sidebar( 'above-footer-widgets-area' ); ?>
-		</div>
-	</div>
-<?php endif; ?>
